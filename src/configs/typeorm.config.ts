efc45a380d52b3905e769a7c325cb3e4a213663c
estherceciliaS <esther.oliveira@natahouse.com>
2023-01-16 23:45:33 -0300

    fix: ajust config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 5435,
   username: 'pguser',
   password: 'pgpassword',
